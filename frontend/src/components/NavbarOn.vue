5810684b1cef92edb3cf86006ce27ef00f7fecb0
davi-alcantara01 <davi.alcantara.ofc@gmail.com>
2025-05-29 22:28:33 -0300

    Geração de token
    Geração de token para chats

--- frontend/src/components/NavbarOn.vue
+++ frontend/src/components/NavbarOn.vue
@@ -16,6 +16,9 @@
             <li class="nav-item">
               <router-link class="nav-link" to="/chats/create">Create chat</router-link>
             </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/chats/enter">Enter chat</router-link>
+            </li>
           </ul>
         </div>
       </div>
--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -6,11 +6,15 @@
       <thead>
         <tr>
           <th scope="col">Chat Name</th>
+          <th scope="col">Token</th>
+          <th scope="col">Quit chat</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="chat in chats" :key="chat.id">
           <td>{{ chat.chat_name }}</td>
+          <td> <button @click="generateToken(chat.id)" class="btn btn-success">Generate token</button> </td>
+          <td> <button class="btn btn-danger">Exit chat</button></td>
         </tr>
       </tbody>
     </table>
@@ -39,8 +43,16 @@ export default {
       console.log("Error: " + error);
       this.$router.push({name: 'login'})
     }
-    
-
+  },
+  methods: {
+    async generateToken(chat_id) {
+      try {
+        let token = await axios.post('http://localhost:3000/chats/token', {id: chat_id});
+        console.log(token.data.token);
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 }
 </script>
